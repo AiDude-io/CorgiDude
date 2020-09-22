181231ddbda22fd6a9ee2b197312f7551f90a1ac
apinuntong <apinun.tong@gmail.com>
2020-09-22 16:34:52 +0700

    ADD
    เพิ่มตรวจจับหน้า mask

---
+++ models/New folder/f_mask_yolo.py
@@ -0,0 +1,31 @@
+import sensor,image,lcd,time
+import KPU as kpu
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_windowing((224, 224))
+sensor.set_vflip(1)
+sensor.run(1)
+lcd.init(type=2, freq=20000000, color=lcd.BLACK)
+lcd.rotation(2)
+clock = time.clock()
+classes = ["unmask","mask"]
+task = kpu.load(0x600000)
+a = kpu.set_outputs(task, 0, 7,7,35)
+anchor = (0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828)
+a = kpu.init_yolo2(task, 0.4, 0.3, 5, anchor)
+while(True):
+    img = sensor.snapshot()
+    #a= img.resize(224,224)
+    a = img.pix_to_ai()
+    code = kpu.run_yolo2(task, img)
+    if code:
+        for i in code:
+            if i.classid() == 0 :
+                a=img.draw_rectangle(i.rect(),color = (255, 0, 0),thickness=4)
+            if i.classid() == 1 :
+                a=img.draw_rectangle(i.rect(),color = (0, 255, 0),thickness=4)
+    a = lcd.display(img)
+
+a = kpu.deinit(task)
