1773e46bca3e53bfcb88fc009d14712a5c060724
comdet <listzone@hotmail.com>
2021-01-07 20:36:59 +0700

    add facemask detection v2

---
+++ 11 - FaceMaskDetection-with-k3-thermometer/Main.py
@@ -0,0 +1,120 @@
+import sensor,image,lcd,time
+import KPU as kpu
+from Maix import I2S, GPIO
+from fpioa_manager import fm
+from machine import UART
+import struct
+from Player import Player
+from time import sleep_ms, ticks_ms, ticks_diff
+######## UART for Temperature
+fm.register (2, fm.fpioa.UART1_TX)
+fm.register (3, fm.fpioa.UART1_RX)
+uart_temp = UART (UART.UART1, 115200, 8, None, 1, timeout = 1000, read_buf_len = 4096)
+######## UART for MP3 Player
+fm.register (21, fm.fpioa.UART2_TX)
+fm.register (20, fm.fpioa.UART2_RX)
+uart_sound = UART (UART.UART2, 9600, 8, None, 1, timeout = 1000, read_buf_len = 4096)
+######## GPIO For trig thermometer
+fm.register(10,  fm.fpioa.GPIO1, force=True)
+triger=GPIO(GPIO.GPIO1,GPIO.OUT)
+triger.value(0)
+
+player = Player(uart_sound,volume= 1)
+
+######## Config Camera and Display
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_windowing((320, 224))
+sensor.set_vflip(0)
+sensor.run(1)
+lcd.init(type=1, freq=15000000, color=lcd.BLACK)
+lcd.rotation(2)
+
+######### config facemask detection
+task = kpu.load(0x400000)
+a = kpu.set_outputs(task, 0, 10,7,35)
+anchor = (0.212104,0.261834, 0.630488,0.706821, 1.264643,1.396262, 2.360058,2.507915, 4.348460,4.007944)
+a = kpu.init_yolo2(task, 0.5, 0.5, 5, anchor)
+######### config detection
+stage = 0
+# 0 = init
+# 1 = wait for user waring mask
+# 2 = ask for approching
+# 3 = wait for approching
+# 4 = check temperature
+
+# time out unmask
+timestamp = 0
+timeout_wait_mask = 5000 # ms
+timeout_wait_approch = 5000 # ms
+timeout_wait_check = 6000 # ms
+sleep_between_person = 6 # sec
+face_size_threshold_w = 80
+face_size_threshold_h = 80
+
+while(True):
+    img = sensor.snapshot()
+    a = img.pix_to_ai()
+    # face detection
+    faces = kpu.run_yolo2(task, img)
+    classid = -1
+    if faces: #found face in screen
+        face = faces[0] # first face only
+        classid = face.classid()
+        x,y,w,h = face.rect()
+        if classid == 0:
+            a=img.draw_rectangle(face.rect(),color = (255, 0, 0),thickness=5)
+        else:
+            a=img.draw_rectangle(face.rect(),color = (0, 255, 0),thickness=5)
+
+    #--------- stage
+    if classid == 0 and stage == 0: # detect face not wearing mask
+        player.play(0, 1)
+        stage = 1
+        timestamp = time.ticks_ms()#stamp time
+    elif stage == 1: # wait for user waring mask
+        if classid == 1: #user wearing mask
+            stage = 2
+            timestamp = time.ticks_ms()#stamp time
+        elif time.ticks_ms() - timestamp > timeout_wait_mask:
+            stage = 0 # reset
+    elif classid == 1 and (stage == 2 or stage == 0): # ask for approching
+        player.play(0, 2)
+        stage = 3
+        timestamp = time.ticks_ms()#stamp time
+    elif classid == 1 and stage == 3: # wait for approching
+        if w > face_size_threshold_w and h > face_size_threshold_h:
+            stage = 4
+            timestamp = time.ticks_ms()#stamp time
+        elif time.ticks_ms() - timestamp > timeout_wait_approch:
+            stage = 0 # reset
+    elif stage == 4: # check temperature
+        a = img.draw_circle(112+49,112+8,50,(128, 128, 128), 3)
+        #####################################
+        triger.value(1)
+        time.sleep(0.005)
+        triger.value(0)
+        rx = uart_temp.read(7)
+        if rx != None :
+            if rx[0] == 85 and rx[1] == 170 and rx[2] == 7 and rx[3] == 4 :
+                temperature = int(struct.unpack("<H",rx[4:6])[0])/10
+                print(temperature)
+                if temperature < 37.5 :
+                    player.play(0, 3)
+                    stage = 0 # reset
+                elif temperature >= 37.5 :
+                    player.play(0, 4)
+                    stage = 0 # reset
+                print("----------")
+                time.sleep(sleep_between_person)
+                img.draw_string(2,2, ("%2.1f C" %(temperature)), color=(0,255,0), scale=4)
+        ######################################
+        if time.ticks_ms() - timestamp > timeout_wait_check:
+            stage = 0 # reset
+    #---------------
+    print("Stage : %d , Timeout %d" %(stage,time.ticks_ms() - timestamp))
+    a = lcd.display(img)
+
+a = kpu.deinit(task)
+
---
+++ 11 - FaceMaskDetection-with-k3-thermometer/Player.py
@@ -0,0 +1,114 @@
+from time import sleep_ms, ticks_ms, ticks_diff
+
+Start_Byte = 0x7E
+Version_Byte = 0xFF
+Command_Length = 0x06
+Acknowledge = 0x00
+End_Byte = 0xEF
+
+# inherent delays in DFPlayer
+CONFIG_LATENCY = 1000
+PLAY_LATENCY =   500
+VOLUME_LATENCY = 500
+
+def clamp(x, minimum, maximum):
+    return max(minimum, min(x, maximum))
+
+def split(num):
+    return num >> 8, num & 0xFF
+
+def kill_time(stamp_ms, kill_ms):
+    diff_ms = ticks_diff(ticks_ms(), stamp_ms)
+    if diff_ms < kill_ms:
+        snooze_ms = kill_ms - diff_ms
+        sleep_ms(snooze_ms)
+        return snooze_ms
+    else:
+        return 0
+
+class Player():
+    def __init__(self, uart=None, busy_pin=None, config=True, volume=1):
+        self._volume = None
+        if uart is None:
+            self.uart.init(9600, bits=8, parity=None, stop=1)
+        else:
+            self.uart = uart
+
+        if config:
+            self.config()
+        if volume is not None:
+            self.volume(volume)
+
+    def command(self, CMD, Par1, Par2):
+        self.awaitconfig()
+        Checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2)
+        HighByte, LowByte = split(Checksum)
+        CommandLine = bytes([b & 0xFF for b in [
+            Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,
+            Par1, Par2, HighByte, LowByte, End_Byte
+        ]])
+        self.uart.write(CommandLine)
+
+    def config(self):
+        self.configtime = ticks_ms()
+        self.reset()
+        self.command(0x3F, 0x00, 0x00)
+
+    def play(self, folderNum, trackNum):
+        self.awaitconfig()
+        self.playtime = ticks_ms()
+        self.command(0x03, folderNum, trackNum)
+
+    def finish(self, folderNum, trackNum):
+        self.play(folderNum, trackNum)
+        while self.playing():
+            sleep_ms(50)
+
+    def awaitconfig(self):
+        if self.configtime is not None:
+            kill_time(self.configtime, CONFIG_LATENCY)
+        self.configtime = None
+
+    def awaitplay(self):
+        if self.playtime is not None: # handle delay between playing and registering
+            kill_time(self.playtime, PLAY_LATENCY)
+        self.playtime = None
+
+    def awaitvolume(self):
+        if self.volumetime is not None: # handle delay between playing and registering
+            kill_time(self.volumetime, VOLUME_LATENCY)
+        self.volumetime = None
+
+    def repeat(self, repeat=True):
+        self.awaitconfig()
+        val = 1 if repeat else 0
+        self.command(0x11, 0, val)
+
+    def _gain(self, gain=1.0):
+        self.awaitconfig()
+        gain = float(clamp(gain, 0, 1.0))
+        val = int(30.0 * gain)
+        self.command(0x10,0 ,val)
+
+    def volume(self, volume=None):
+        self.awaitconfig()
+        if volume is None:
+            return self._volume
+        else:
+            self._volume = float(clamp(volume, 0, 1.0))
+            val = int(30.0 * self._volume)
+            self.command(0x06,0 ,val)
+            self.volumetime = ticks_ms()
+
+    def standby(self):
+        self.awaitconfig()
+        self.command(0x0A, 0x00, 0x00)
+
+    def wake(self):
+        self.awaitconfig()
+        self.command(0x0B, 0x00, 0x00)
+
+    def reset(self):
+        self.awaitconfig()
+        self.command(0x0C, 0x00, 0x00)
+#///////////////////////////////////////////////////////////////////////////
